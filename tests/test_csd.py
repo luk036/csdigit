907395721a1efb50eea488ec315f1d0faaadd28f
Wai-Shing Luk <luk036@gmail.com>
2023-02-25 13:43:46 +0800

    better code coverage

--- tests/test_csd.py
+++ tests/test_csd.py
@@ -2,6 +2,7 @@ from hypothesis import given
 from hypothesis.strategies import integers
 
 from csdigit.csd import to_csd, to_csd_i, to_decimal, to_decimal_i
+from csdigit.csd import to_csdfixed
 
 
 def test_csd_s():
@@ -18,3 +19,8 @@ def test_csd_i(number):
 def test_csd(number):
     fnum = number / 8
     assert fnum == to_decimal(to_csd(fnum, 4))
+
+
+def test_to_csdfixed():
+    assert to_csdfixed(28.5, 4) == "+00-00.+"
+    assert to_csdfixed(-0.5, 4) == "0.-"
---
+++ tests/test_lcsre.py
@@ -0,0 +1,5 @@
+from csdigit.lcsre import longest_repeated_substring
+
+
+def test_lcsre():
+    assert longest_repeated_substring("+-00+-00+-00+-0") == "+-00+-0"
