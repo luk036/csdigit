2895a99db0db0bba52e8c40077d155c3b623a7c0
Wai-Shing Luk <luk036@gmail.com>
2023-07-16 18:29:08 +0800

    use readthedoc theme

--- tests/test_csd.py
+++ tests/test_csd.py
@@ -2,12 +2,30 @@ from hypothesis import given
 from hypothesis.strategies import integers
 
 from csdigit.csd import to_csd, to_csd_i, to_decimal, to_decimal_i
+from csdigit.csd import to_decimal_using_pow
 from csdigit.csd import to_csdfixed
+
+
+def test_for():
+    for i in range(6):
+        print(i)
+    else:
+        print("nothing")
 
 
 def test_csd_s():
     number = -342343593459544395894535439534985
     assert number == to_decimal_i(to_csd_i(number))
+
+
+def test_to_csdfixed():
+    assert to_csdfixed(28.5, 4) == "+00-00.+"
+    assert to_csdfixed(-0.5, 4) == "0.-"
+
+
+def test_to_csd_invalid():
+    csd_string = "++00-00ZZZZ.+"
+    # TODO: check how to test
 
 
 @given(integers())
@@ -21,6 +39,7 @@ def test_csd(number):
     assert fnum == to_decimal(to_csd(fnum, 4))
 
 
-def test_to_csdfixed():
-    assert to_csdfixed(28.5, 4) == "+00-00.+"
-    assert to_csdfixed(-0.5, 4) == "0.-"
+@given(integers())
+def test_csd_using_pow(number):
+    fnum = number / 8
+    assert fnum == to_decimal_using_pow(to_csd(fnum, 4))
