f4631a926429fb7a0581d38927c18ba0727dfe8c
Wai-Shing Luk <luk036@gmail.com>
2024-01-27 11:05:28 +0800

    add more regression

--- tests/test_lcsre.py
+++ tests/test_lcsre.py
@@ -4,3 +4,20 @@ from csdigit.lcsre import longest_repeated_substring
 def test_lcsre():
     assert longest_repeated_substring("+-00+-00+-00+-0") == "+-00+-0"
     assert longest_repeated_substring("abcdefgh") == ""
+
+
+def test_longest_repeated_substring():
+    # Test case 1:
+    cs = "+-00+-00+-00+-0"
+    expected = "+-00+-0"
+    assert longest_repeated_substring(cs) == expected
+
+    # Test case 2:
+    cs = "abcabcabc"
+    expected = "abc"
+    assert longest_repeated_substring(cs) == expected
+
+    # Test case 3:
+    cs = "abcdefghijklmnopqrstuvwxyz"
+    expected = ""
+    assert longest_repeated_substring(cs) == expected
