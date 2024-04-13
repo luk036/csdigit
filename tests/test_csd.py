a6dbce3a9658a0f7c8991712837d9037f07509f8
Wai-Shing Luk <luk036@gmail.com>
2024-04-13 06:20:57 +0000

    apply pre-commit

--- tests/test_csd.py
+++ tests/test_csd.py
@@ -2,12 +2,11 @@ import pytest
 from hypothesis import given
 from hypothesis.strategies import integers
 
-from csdigit.csd import (
+from csdigit.csd import (  # to_decimal_i,
     to_csd,
     to_csd_i,
     to_csdfixed,
     to_decimal,
-    # to_decimal_i,
     to_decimal_using_pow,
 )
 
