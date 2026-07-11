09da08aeb7ad84116cd96fc390987512a897e2de
Wai-Shing Luk <luk036@gmail.com>
2026-07-11 23:30:01 +0800

    Add CSD benchmark and verification scripts
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ benches/bench_csd.py
@@ -0,0 +1,20 @@
+import timeit
+from csdigit.csd import to_csd, to_csd_i, to_decimal, to_decimal_using_pow, to_csdnnz
+
+def bench(name, stmt, globals_dict=None, number=50000):
+    if globals_dict is None:
+        globals_dict = globals()
+    t = timeit.timeit(stmt, globals=globals_dict, number=number)
+    ns_per_op = t * 1e9 / number
+    print(f"  {name:<30} {ns_per_op:>8.1f} ns/op  ({number} iters)")
+
+print("=== CSD Benchmarks (Python) ===")
+bench("to_csd(28.5, 10)", "to_csd(28.5, 10)")
+bench("to_csd_i(28)", "to_csd_i(28)")
+bench("to_csdnnz(28.5, 4)", "to_csdnnz(28.5, 4)")
+bench("to_decimal('+00-00.+0')", "to_decimal('+00-00.+0')")
+bench("to_decimal_using_pow('+00-00')", "to_decimal_using_pow('+00-00')")
+bench("to_csd(0.0, 10) [zero]", "to_csd(0.0, 10)")
+bench("to_csd(-28.5, 10) [neg]", "to_csd(-28.5, 10)")
+bench("to_csd(0.5, 10) [small]", "to_csd(0.5, 10)")
+bench("to_csd(1024.75, 10) [large]", "to_csd(1024.75, 10)")
---
+++ benches/verify_output.py
@@ -0,0 +1,36 @@
+from csdigit.csd import to_csd, to_csd_i, to_decimal, to_decimal_using_pow, to_csdnnz, to_csdnnz_i
+
+print("=== to_csd (float) ===")
+for v, p in [(28.5,2), (-0.5,2), (0.0,2), (2.5,4), (0.0,0), (7.0,0), (-7.0,0), (28.5,0)]:
+    res = to_csd(v, p)
+    print(f'  to_csd({v}, {p}) = "{res}"')
+
+print()
+print("=== to_csd_i (integer) ===")
+for v in [28, 0, -28, 7, -7, 1, -1]:
+    res = to_csd_i(v)
+    print(f'  to_csd_i({v}) = "{res}"')
+
+print()
+print("=== to_decimal ===")
+for s in ["+00-00.+", "0.-", "0", "0.0", "0.+", "0.-", "0.++", "0.-+", "+00-00"]:
+    res = to_decimal(s)
+    print(f'  to_decimal("{s}") = {res}')
+
+print()
+print("=== to_decimal_using_pow (integer CSD) ===")
+for s in ["+00-00", "0", "-00+00"]:
+    res = to_decimal_using_pow(s)
+    print(f'  to_decimal_using_pow("{s}") = {res}')
+
+print()
+print("=== to_csdnnz (float, limited nnz) ===")
+for v, n in [(28.5,4), (-0.5,4), (0.0,4), (0.5,4), (0.0,0), (28.5,2), (28.5,1)]:
+    res = to_csdnnz(v, n)
+    print(f'  to_csdnnz({v}, {n}) = "{res}"')
+
+print()
+print("=== to_csdnnz_i (integer, limited nnz) ===")
+for v, n in [(28,4), (0,4)]:
+    res = to_csdnnz_i(v, n)
+    print(f'  to_csdnnz_i({v}, {n}) = "{res}"')
